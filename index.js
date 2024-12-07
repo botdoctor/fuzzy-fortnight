ded496646e56dece119b80a85b8abf8a573f9a4a
sigbender69 <ichicken4321@gmail.com>
2024-12-06 19:02:51 -0600

    various changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function hello(){
     alert('hello world welcome to joe kleves final');
+    document.querySelector('h1').innerHTML = 'Goodbye';
 }
 
 
@@ -7,3 +8,12 @@ function test(){
 alert('testing')
 }
 
+function displayInput() {
+    const input = document.getElementById('userInput').value;
+    const output = document.getElementById('output');
+    if (input.trim()) {
+        output.textContent = `You entered: ${input}`;
+    } else {
+        output.textContent = 'Please enter something!';
+    }
+}
